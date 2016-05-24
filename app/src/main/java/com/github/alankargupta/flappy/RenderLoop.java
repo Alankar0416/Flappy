79042921caf49f825ad0743150a368393cf491fc
Alankar Gupta <alankargupta@urbanclap.com>
2016-05-24 10:59:02 +0530

    Creating RenderLoop class and deleting test packages

--- app/src/main/java/com/github/alankargupta/flappy/RenderLoop.java
+++ app/src/main/java/com/github/alankargupta/flappy/RenderLoop.java
@@ -3,13 +3,11 @@ package com.github.alankargupta.flappy;
  * Created by alankargupta on 24/05/16.
  */
 
+//To run GameSurface periodically at a regular interval
 public class RenderLoop implements Runnable {
-
-
 
     @Override
     public void run() {
-
 
         /*while(true){
 
